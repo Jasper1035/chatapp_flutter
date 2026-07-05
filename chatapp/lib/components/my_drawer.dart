a81ade04ce76e90300fccf1f86df2238acdb405d
Jasper1035 <104673426+Jasper1035@users.noreply.github.com>
2026-07-05 06:47:38 +0100

    add drawer

---
+++ chatapp/lib/components/my_drawer.dart
@@ -0,0 +1,73 @@
+import 'package:chatapp/auth/auth_service.dart';
+import 'package:chatapp/pages/settings.dart';
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({super.key});
+
+  void logout() {
+    final _auth = AuthService();
+    _auth.signOut();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            children: [
+              //logo
+              DrawerHeader(
+                child: Center(
+                  child: Icon(
+                    Icons.message,
+                    color: Theme.of(context).colorScheme.primary,
+                    size: 50,
+                  ),
+                ),
+              ),
+              //home list Tile
+              Padding(
+                padding: EdgeInsets.only(left: 25),
+                child: ListTile(
+                  title: Text('H O M E'),
+                  leading: Icon(Icons.home),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
+              // settings list Tile
+              Padding(
+                padding: EdgeInsets.only(left: 25),
+                child: ListTile(
+                  title: Text('S E T T I N G S'),
+                  leading: Icon(Icons.settings),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Settings()),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+
+          // log out list tile
+          Padding(
+            padding: EdgeInsets.only(left: 25, bottom: 25),
+            child: ListTile(
+              title: Text('L O G O U T'),
+              leading: Icon(Icons.logout),
+              onTap: logout,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- chatapp/lib/pages/home.dart
+++ chatapp/lib/pages/home.dart
@@ -1,21 +1,15 @@
-import 'package:chatapp/auth/auth_service.dart';
+// import 'package:chatapp/auth/auth_service.dart';
+import 'package:chatapp/components/my_drawer.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
-  void logout() {
-    final _auth = AuthService();
-    _auth.signOut();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-        actions: [IconButton(onPressed: logout, icon: Icon(Icons.logout))],
-      ),
+      appBar: AppBar(title: Text('Home')),
+      drawer: MyDrawer(),
     );
   }
 }
